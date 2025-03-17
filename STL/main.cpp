7daf8769a7265b162ed21f9332b4113c89e10e05
popopocorn <popcorn375@naver.com>
2025-03-17 19:01:36 +0900

    2주2일차

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,51 +2,23 @@
 // 2025/1학기 STL
 // 3.10 월요일	(1주차 2일)
 //----------------------------------------------------------------------------
-// Generic Programming - 핵심키워드 Template
+// 많은 수의 자료 다루기 - int -> Dog
 //----------------------------------------------------------------------------
 #include<iostream>
 #include"save.h"
 
+#include<fstream>
 
-// template은 선언과 정의를 동시에 해야함
-template<typename t> // 
-void change(t& x, t& y); 
-class Dog {
-public:
-	Dog() = default;
-	Dog(int n) : age{ n } {}
-	friend std::ostream& operator<<(std::ostream& os, const Dog& d) {
-		return os << d.age;
-	}
-private:
-	int age;
-};
-
+// [문제] "int 1000개.txt"파일에서 가장  큰 값을 찾아 화면에 출력하라
+//출력된 값도 답지에 적어라
 int main() {
-
-	{
-		Dog a{ 1 }, b{ 2 };
-		change(a, b); //뭘 실행하는거임
-		//1.change(Dog, Dog)
-		//2.change(Dog&, Dog&)
-		//3.컴파일러가 직접 생성 - template
-
-		std::cout << a << ", " << b << std::endl;
+	std::ifstream in{ "int 1000개.txt" };
+	if (not in) {
+		std::cout << "파일을 찾지 못했습니다.\n";
 	}
 
-	{
-		int a{ 1 }, b{ 2 };
-		change(a, b); //ab바꾸기
-
-		std::cout << a << ", " << b << std::endl;
-	}
 
 	save("main.cpp");
 
 }
-template<typename t>
-void change(t& x, t& y) {
-	t temp{ x };
-	x = y;
-	y = temp;
-}
+
