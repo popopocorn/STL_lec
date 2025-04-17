60f7e90f6ca99798f17fc0f793c9cd07b9227d84
popopocorn <popcorn375@naver.com>
2025-04-17 19:02:22 +0900

    0417

--- STL/STRING.cpp
+++ STL/STRING.cpp
@@ -1,75 +1,140 @@
-//-------------------------------------------------------------------
-// STRING.c++ STL의 동작을 이해하기 위한 클래스 / std::stirng과 유사하게
-// 2025/4/10시작
-// 2024/4/10 - 복사생성/복사할당, 스페셜함수의 동작관찰
-//-------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// STRING.cpp	- STL의 동작을 이해하기 위한 class / std::string과 유사하게 작성	
+//
+// 2025. 4. 10 - 시작		
+// 2025. 4. 10 - 복사생성/복사할당연산자, 스페셜 함수의 동작 관찰	
+// 2025. 4. 14 - 선택적 관찰하도록 전역변수를 사용	
+// 2025. 4. 14 - 이동의미론(move semantics) 구현 	
+//-----------------------------------------------------------------------------
+#include <memory>
+#include <print>
+#include "STRING.h"
 
-#include<memory>
-#include<print>
-#include"STRING.h"
-
-size_t STRING::gid{ 0 };	//2025/4/10 고유 번호 생성
+size_t STRING::gid{ 0 };			// 2025. 4. 10 고유번호 생성
+bool 관찰{ false };
 
 STRING::STRING()
-	: id{++gid}
+	: id{ ++gid }
 {
-	std::println("[{:6}]  {:<16} 자원수: {:4}, 주소: {:16} 자원의 주소: {:16}",
-		id, " 디폴트 생성 ", len, (void*)this, (void*)str.get());
-	//		  이동할당연산자
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "디폴트생성", num, (void*)this, (void*)p.get());
+	}
 }
 
 STRING::~STRING()
 {
-	std::println("[{:6}]  {:<16} 자원수: {:4}, 주소: {:16} 자원의 주소: {:16}",
-		id, " 소멸자 ", len, (void*)this, (void*)str.get());
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "소멸자", num, (void*)this, (void*)p.get());
+	}
 }
 
-
-STRING::STRING(const char* c) 
-	: len{ strlen(c) }, id{ ++gid }
-{	
-		str.release();							
-		str = std::make_unique<char[]>(len);
-		memcpy(str.get(), c, len);				
-		std::println("[{:6}]  {:<16} 자원수: {:4}, 주소: {:16} 자원의 주소: {:16}",
-			id, " 생성자(char*) ", len, (void*)this, (void*)str.get());
-}
-
-STRING::STRING(const STRING& other) :
-	len{ other.len }, id{ ++gid }
-
+STRING::STRING(const char* str)
+	: num{ strlen(str) }, id{ ++gid }
 {
-	str.release();
-	str = std::make_unique<char[]>(len);
-	memcpy(str.get(), other.str.get(), len);
-	std::println("[{:6}]  {:<16} 자원수: {:4}, 주소: {:16} 자원의 주소: {:16}",
-		id, " 복사 생성 ", len, (void*)this, (void*)str.get());
+	p.release();
+	p = std::make_unique<char[]>(num);
+	memcpy(p.get(), str, num);
+
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "생성자(char*)", num, (void*)this, (void*)p.get());
+	}
+};
+
+// 복사생성자와 복사할당연산자  2025. 4. 10
+STRING::STRING(const STRING& other)
+	: num{ other.num }, id{ ++gid }
+{
+	p = std::make_unique<char[]>(num);
+	memcpy(p.get(), other.p.get(), num);
+
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "복사생성자", num, (void*)this, (void*)p.get());
+	}
 }
 
-
-STRING& STRING::operator=(const STRING& other) 
+STRING& STRING::operator=(const STRING& other)
 {
-	if (this == &other) {
+	if (this == &other)
 		return *this;
+
+	num = other.num;
+
+	p.release();
+	p = std::make_unique<char[]>(num);
+	memcpy(p.get(), other.p.get(), num);
+
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "copy operator=", num, (void*)this, (void*)p.get());
 	}
 
-	len = other.len;
-	str.release();
-	str = std::make_unique<char[]>(len);
-	memcpy(str.get(), other.str.get(), len);
-	std::println("[{:6}]  {:<16} 자원수: {:4}, 주소: {:16} 자원의 주소: {:16}",
-		id, " copy operator= ", len, (void*)this, (void*)str.get());
 	return *this;
 }
 
-size_t STRING::size() const {
-		return len;
+// 이동생성자와 이동할당연산자	- 2025. 4. 14
+STRING::STRING(STRING&& other)
+	: num{ other.num }, id{ ++gid }
+{
+	p.reset(other.p.release());
+
+	//	other.num = 0;			// 안 해도 된다
+
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "이동생성자", num, (void*)this, (void*)p.get());
+	}
 }
 
-std::ostream& operator<<(std::ostream& os, const STRING& s) {
-		for (int i = 0; i < s.len; ++i) {
-			os << s.str[i];
-		}
-		return os;
+STRING& STRING::operator=(STRING&& other)
+{
+	if (this == &other)
+		return *this;
+
+	num = other.num;
+	p.release();		// 내가 확보한 자원을 해제
+	p.reset(other.p.release());
+
+	//	other.num = 0;			// 안 해도 됨
+
+	if (관찰) {
+		std::println("[{:6}] {:<16} 자원수:{:4}, 주소:{:16} 자원의 주소:{:16}",
+			id, "move operator=", num, (void*)this, (void*)p.get());
+	}
+	return *this;
 }
 
+
+// 기본정렬을 위한 < - 2025. 4. 14
+bool STRING::operator<(const STRING& rhs) const
+{
+	return size() < rhs.size();
+}
+
+size_t STRING::size() const
+{
+	return num;
+}
+
+std::ostream& operator<<(std::ostream& os, const STRING& s)
+{
+	for (int i = 0; i < s.num; ++i)
+		os << s.p[i];
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, STRING& s) 		// 2025. 4. 14
+{
+	std::string str;
+	is >> str;
+
+	s.num = str.size();
+	s.p.release();
+	s.p = std::make_unique<char[]>(s.num);
+	memcpy(s.p.get(), str.data(), s.num);
+
+	return is;
+}
--- STL/main.cpp
+++ STL/main.cpp
@@ -1,28 +1,35 @@
 //----------------------------------------------------------------------------
 // 2025/1학기 STL
 //----------------------------------------------------------------------------
-// class STRING 작성 시작 - STL의 표준 컨테이너가 되도록 발전 시킨다
-//							벡터의 동작을 진짜 자세히 이해한다
+//	STL container - Containers are objects that store other objects.
 //----------------------------------------------------------------------------
 #include<iostream>
 #include"save.h"
 #include"STRING.h"
 using namespace std;
 //----------------------------------------------------------------------------
-#include<string>
-#include<memory>
+#include<vector>
 //----------------------------------------------------------------------------
 
+extern bool 관찰;
 
-//다음시간 관찰 메시지 해석
-STRING x{ "수업시간이너무짧아" };
+
 
 int main() {
 
-	STRING s{ "nanananananan" };
+	vector<int> v;
 
-	STRING t;
-	STRING u = s;
+	cout << "원소 개수 - " << v.size() << endl;
+	cout << "담을 수 있는 원소 개수 - " << v.capacity() << endl;
+	cout << "자원 주수 - " << v.data() << endl;
+
+	cout << endl;
+	cout << "원소 한 개 추가 한 후 vector v의 field" << endl;
+	v.push_back(1);
+
+	cout << "원소 개수 - " << v.size() << endl;
+	cout << "담을 수 있는 원소 개수 - " << v.capacity() << endl;
+	cout << "자원 주수 - " << v.data() << endl;
 
 	save("main.cpp");
 }
