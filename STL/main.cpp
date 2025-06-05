03b4e1e52e76c36011856646b2c442d4fa4d85c5
popopocorn <popcorn375@naver.com>
2025-06-05 19:04:05 +0900

    0605

--- STL/main.cpp
+++ STL/main.cpp
@@ -2,52 +2,26 @@
 // 2025 / 1학기 STL
 // 6/19 목요일 15주2일 - 기말시험
 //----------------------------------------------------------------------------
-// associative container
-//	- set<key>			: key와 value를 구분하지 않는 컨테이너
-//	- map<Key, Value>	: 아이돌 그룹 - 멤버
-//						  모델링 - 텍스처
+// unordered associative container - hash 기반
 //----------------------------------------------------------------------------
 #include<iostream>
 #include"save.h"
 #include"STRING.h"
 using namespace std;
 //----------------------------------------------------------------------------
-#include<fstream>
-#include<array>
-#include<set>
+#include<unordered_set>
+#include<string>
 //----------------------------------------------------------------------------
 
 extern bool 관찰;
 
 
 int main() {
-	
-	ifstream in{ "이상한 나라의 앨리스.txt" };
-	if (not in)
-	{
-		cout << "파일 읽기 실패\n\n";
-		return -20250602;
-	}
-	
-	multiset<STRING, less<STRING>> s{ istream_iterator<STRING>{in}, {} }; // 사용하지 말것
+	unordered_set<string> us{"1", "22", "333", "4444", "55555"};
+	us.insert("666666");
+	for (const string& s : us)
+		cout << s << endl;
+	cout << std::hash<string>{}("666666") << endl;
+	cout << std::hash<string>{}("4444") << endl;
 	save("main.cpp");
-	// [문제] 멀티셋에 저장된 모든 알파벳의 사용 횟수를 다음과 같이 출력하라
-	// 대문자는 소문자로 바꾸어 취급한다
-	// a - 320
-	// b - 645
-	// ...
-	// z - 2
-	array<size_t, 26> alphaNum{};
-	for (const STRING w : s) {
-		for (char c : w) {
-			if(isalpha(c)){
-				c = tolower(c);
-				alphaNum[c - 'a']++;
-			}
-		}
-	}
-	// 출현횟수 내림차순으로 출력하라
-	for (int i = 0; i < alphaNum.size(); ++i) {
-		cout << char('a' + i) << " - " << alphaNum[i] << endl;
-	}
 }
